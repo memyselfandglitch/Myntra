f54fe7b3644647a1860c2c857ff522809824c4d8
Deveshi Singh <deveshi@Deveshis-Laptop.local>
2024-07-15 20:13:23 +0530

    Typo

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
-          Login Page
+          Let's get started
         </Typography>
         <Form />
       </Box>
